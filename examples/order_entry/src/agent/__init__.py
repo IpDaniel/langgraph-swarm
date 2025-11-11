78fd044f4018840871b4fd233caabb2f0d393c01
IpDaniel <ip.d@northeastern.edu>
2025-11-10 22:31:46 -0500

    updated to do list

--- examples/order_entry/src/agent/__init__.py
+++ examples/order_entry/src/agent/__init__.py
@@ -1,2 +1,16 @@
 """Order entry agent package."""
-
+"""
+Things I need to remember to do:
+- Make a normal repo
+- Make some kind of 'new conversation' route that will initialize a new conversation
+  with a new CustomerContext based on the input value
+- Add another attribute to CustomerContext for things that don't change
+- Integrate the context manager agent into the rest of the graph
+- Get S3 client to install properly
+- Implement splitting tool (again ugh)
+- Implement merging tool
+- Implement splitting checker tool (interesting)
+- Implement checklist part of state and tools to go with it? Let the parameter entry 
+  agent figure out what to do about it
+- Figure out the pattern to keep state from randomly resetting
+"""
